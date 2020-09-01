128db6b430af01865660898365d311667635760a
Raphael Ribeiro <raphael.ribeiro.pro@gmail.com>
2020-09-01 16:20:07 +0200

    Adding Delay and header guard

--- src/lib.h
+++ src/lib.h
@@ -1,3 +1,11 @@
+#ifndef LIB_H_
+#define LIB_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+void waitFor(unsigned int secs);
 void FreeGrid(char **grid, int gridWidth);
 char **CreateGrid(int gridWidth);
 char **CopyGrid(char **grid, int gridWidth);
@@ -5,3 +13,5 @@ void NewGeneration(char **grid, int gridWidth);
 void PopulateCells(char **grid, int gridWidth, int rate);
 void PrintGrid(char **grid, int gridWidth);
 void PrintIntro();
+
+#endif /* !LIB_H_ */
